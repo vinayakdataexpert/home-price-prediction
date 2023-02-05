e6038d101f5c2d5c5b007197319aa96ca79ff9c2
Vinayak Pandey <vinayak.dataexpert@gmail.com>
2023-02-05 09:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ bathroom = st.number_input('Enter Number of Bathroom')
 bhk = st.number_input('Enter Number of Bedroom')
 
 if st.button('Predict'):
-    input_df = pd.DataFrame({'location': [location_input], 'total_sqft': [total_sqft], 'bath': [bathroom], 'bhk': [bhk]})
+    input_df = pd.DataFrame({'location': location_input, 'total_sqft': total_sqft, 'bath': bathroom, 'bhk': bhk})
     
-    if total_sqft == 0.00 or bath == 0.00 or bhk == 0.00:
+    if total_sqft == 0.00 or bathroom == 0.00 or bhk == 0.00:
         result = "Please type Valid Sqare feet,Bathroom BHK"
         st.header(result)
     else:
